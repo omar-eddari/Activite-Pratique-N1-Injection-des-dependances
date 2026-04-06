859b178d4e910d735e53435961e8892c4fa2e546
Omar eddari <eddari.omar.97@gmail.com>
2026-04-06 12:16:04 +0100

    mini framework

---
+++ src/main/java/framework/core/BeanDefinition.java
@@ -0,0 +1,12 @@
+package framework.core;
+
+import java.util.List;
+
+public class BeanDefinition {
+    private String id;
+    private String className;
+
+    private List<Property> properties;
+
+    // getters & setters
+}
---
+++ src/main/java/framework/core/Property.java
@@ -0,0 +1,8 @@
+package framework.core;
+
+public class Property {
+    private String name;
+    private String ref; // reference to another bean
+
+    // getters & setters
+}
